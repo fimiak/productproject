ef46ba83274906f0e87fd9369055b3384dc0d198
Tyler James Greve <tylerjamesgreve@gmail.com>
2019-04-02 17:07:51 -0700

    Fixed up pricing page, added Font Awesome, work on header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,151 @@
 import React from 'react';
 
 function Footer() {
-  return <footer class="footer">Footer</footer>;
+  return (
+    <footer class="footer">
+      <div className="footer__heading">
+        <span>&#9851; Recycle</span>
+        <span>
+          <input placeholder="Enter Email Address" />
+          <button>Try Now</button>
+        </span>
+      </div>
+      <div className="footer__list">
+        <div className="footer__list-box">
+          <ul>
+            <li>
+              <h4>Business</h4>
+            </li>
+            <li>
+              <a href="/">Warehouses</a>
+            </li>
+            <li>
+              <a href="/">Docs</a>
+            </li>
+            <li>
+              <a href="/">Blog</a>
+            </li>
+            <li>
+              <a href="/">Team</a>
+            </li>
+            <li>
+              <a href="/">Pricing</a>
+            </li>
+            <li>
+              <a href="/">Press</a>
+            </li>
+          </ul>
+          <span>&copy;2019 Recycle</span>
+        </div>
+        <div className="footer__list-box">
+          <ul>
+            <li>
+              <h4>Developers</h4>
+            </li>
+            <li>
+              <a href="/">Help Center</a>
+            </li>
+            <li>
+              <a href="/">Contact</a>
+            </li>
+            <li>
+              <a href="/">Docs</a>
+            </li>
+            <li>
+              <a href="/">Product</a>
+            </li>
+            <li>
+              <a href="/">Contact</a>
+            </li>
+            <li>
+              <a href="/">Partner Portal</a>
+            </li>
+          </ul>
+          <span>
+            <a href="/">
+              <i class="fab fa-facebook-square fa-lg" />
+            </a>
+            &nbsp;
+            <a href="/">
+              <i class="fab fa-twitter-square fa-lg" />
+            </a>
+            &nbsp;
+            <a href="/">
+              <i class="fab fa-linkedin fa-lg" />
+            </a>
+            &nbsp;
+            <a href="/">
+              <i class="fas fa-address-card fa-lg" />
+            </a>
+          </span>
+        </div>
+        <div className="footer__list-box">
+          <ul>
+            <li>
+              <h4>For Business</h4>
+            </li>
+            <li>
+              <a href="/">Help Center</a>
+            </li>
+            <li>
+              <a href="/">Contact</a>
+            </li>
+            <li>
+              <a href="/">Product</a>
+            </li>
+            <li>
+              <a href="/">Contact</a>
+            </li>
+            <li>
+              <a href="/">Partner Portal</a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer__list-box">
+          <ul>
+            <li>
+              <h4>Company</h4>
+            </li>
+            <li>
+              <a href="/">Help Center</a>
+            </li>
+            <li>
+              <a href="/">Contact</a>
+            </li>
+            <li>
+              <a href="/">Contact</a>
+            </li>
+            <li>
+              <a href="/">Partner Portal</a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer__list-box">
+          <ul>
+            <li>
+              <h4>Support</h4>
+            </li>
+            <li>
+              <a href="/">Help Center</a>
+            </li>
+            <li>
+              <a href="/">Careers</a>
+            </li>
+            <li>
+              <a href="/">Contact</a>
+            </li>
+            <li>
+              <a href="/">Partner Portal</a>
+            </li>
+          </ul>
+          <span>
+            <a href="/">Privacy</a>
+            <a href="/">Terms</a>
+          </span>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/components/pages/Pricing.js
+++ src/components/pages/Pricing.js
@@ -7,6 +7,11 @@ class Pricing extends React.Component {
         <div className="pricing__top">
           <div className="pricing__top-bg" />
           <div className="pricing__top-content">
+            <div className="pricing_title">
+              <span>Pricing</span>
+              <h2>Pricing Plans to Fit All Company Needs</h2>
+            </div>
+
             <ul className="pricing__cards">
               <li className="pricing__card">
                 <span>&#9851;</span>
@@ -48,8 +53,103 @@ class Pricing extends React.Component {
             </ul>
           </div>
         </div>
-        <div className="pricing__features">Features</div>
-        <div className="pricing__faq">FAQ</div>
+        <div className="pricing__features">
+          <table>
+            <tbody>
+              <tr>
+                <td>&nbsp;</td>
+                <td>Solo</td>
+                <td>Group</td>
+                <td>Enterprise</td>
+              </tr>
+              <tr className="pricing__spacer">
+                <td colspan="4">Plan</td>
+              </tr>
+              <tr>
+                <td>Pick your plan</td>
+                <td className="pricing__icon">&#10086;</td>
+                <td className="pricing__icon">&#10086;</td>
+                <td className="pricing__icon">&#10086;</td>
+              </tr>
+              <tr className="pricing__spacer">
+                <td colspan="4">Features</td>
+              </tr>
+              <tr>
+                <td>Single Sign-On</td>
+                <td>&nbsp;</td>
+                <td className="pricing__icon">&#10086;</td>
+                <td className="pricing__icon">&#10086;</td>
+              </tr>
+              <tr>
+                <td>Broadcast</td>
+                <td>&nbsp;</td>
+                <td className="pricing__icon">&#10086;</td>
+                <td className="pricing__icon">&#10086;</td>
+              </tr>
+              <tr>
+                <td>Wireless</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td className="pricing__icon">&#10086;</td>
+              </tr>
+              <tr>
+                <td>Email Support</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td className="pricing__icon">&#10086;</td>
+              </tr>
+              <tr>
+                <td>Plan</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>Contact</td>
+              </tr>
+              <tr className="pricing__spacer">
+                <td colspan="4">&nbsp;</td>
+              </tr>
+              <tr>
+                <td>
+                  <h4>Get started</h4>
+                  <span className="pricing__features-text">Try a plan to get started immediately</span>
+                </td>
+                <td>
+                  <h4>Solo</h4>
+                  <button>Create Account</button>
+                </td>
+                <td>
+                  <h4>Group</h4>
+                  <button>Try Free</button>
+                </td>
+                <td>
+                  <h4>Enterprise</h4>
+                  <button>Contact Us</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className="pricing__faq">
+          <h2>Frequently Asked Questions</h2>
+          <div className="pricing__faq-items">
+            <p>
+              <h5>How does it work?</h5>If you are on the monthly billing plan, your bill may fluctuate. Includes Even
+              More Stuff.
+            </p>
+            <p>
+              <h5>How much does it cost?</h5>If you are on the monthly billing plan, your bill may fluctuate. Includes
+              Even More Stuff.
+            </p>
+            <p>
+              <h5>Where are you located?</h5>If you are on the monthly billing plan, your bill may fluctuate. Includes
+              Even More Stuff.
+            </p>
+            <p>
+              <h5>Can I get a refund?</h5>If you are on the monthly billing plan, your bill may fluctuate. Includes Even
+              More Stuff.
+            </p>
+          </div>
+          <button>See the Full FAQ</button>
+        </div>
       </div>
     );
   }
